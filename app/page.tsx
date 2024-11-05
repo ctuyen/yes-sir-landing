4b339531b10c6188cfb5cd9066e74913e0b41118
Tuyen Le <congtuyen598@gmail.com>
2024-11-05 10:01:01 +0700

    done UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,101 +1,60 @@
+import React from "react";
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
-export default function Home() {
+interface YesSirStudioProps {
+  phone: string;
+  email: string;
+}
+
+
+const YesSirStudio: React.FC<YesSirStudioProps> = ({ phone = "+84 70 2845158", email = "hello@yessirstudio.com" }) => {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="min-h-screen w-screen" style={{ background: 'radial-gradient(50% 50% at 50% 50%, #1A1748 0%, #161616 100%)' }}>
+      {/* Header */}
+      <div className="px-4 md:px-12 py-4">
+        {/* Logo */}
+        <div className="p-4">
+          <Image src="/assets/images/logo.svg" alt="YesSirStudio Logo" width={40} height={40} priority />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      {/* Hero Section */}
+      <div className="flex items-center justify-between text-white px-4 md:px-12 mt-4 md:mt-8">
+        <div className="flex flex-col items-start px-4 w-full md:w-7/12 text-left">
+
+          {/* Main Content */}
+          <h2 className="text-sm font-semibold uppercase tracking-wider text-gray-400">
+            Make Better IT Services
+          </h2>
+          <h1 className="text-4xl font-bold my-4">Hello, we are YesSirStudio!</h1>
+          <p className="text-lg font-light text-gray-300">
+            We are a team based in Vietnam, dedicated to delivering the best quality in record time.
+          </p>
+
+          {/* Hero Graphics Mobile */}
+          <Image src="/assets/images/Hero-Image-Section.svg" alt="YesSirStudio Hero Image" className="w-full md:hidden" width={667} height={493} priority />
+
+          {/* Buttons */}
+          <div className="w-full mt-10 flex flex-col md:flex-row md:items-start space-y-4 md:space-y-0 md:space-x-4">
+            <Button className="px-6 py-2 bg-white text-black font-semibold rounded-lg shadow-lg hover:bg-gray-200">Get Quick Quote</Button>
+            <Button variant="outline" className="px-6 py-2 bg-transparent border border-white text-white hover:bg-gray-200 font-semibold rounded-lg">
+              Schedule a quick chat
+            </Button>
+          </div>
+
+          {/* Contact Information */}
+          <div className="mt-12 mb-2 text-sm text-gray-500">
+            <hr className="my-4 border-gray-700" />
+            <p>Tel: <a href={`tel:${phone}`}> {phone} </a> | Email: <a href={`mailto:${email}`}> {email} </a></p>
+            <p>&copy; 2024 YesSirStudio. All rights reserved.</p>
+          </div>
+        </div>
+
+        {/* Hero Graphics Desktop */}
+        <Image src="/assets/images/Hero-Image-Section.svg" alt="YesSirStudio Hero Image" className="w-5/12 hidden md:block" width={667} height={493} priority />
+      </div>
     </div>
   );
-}
+};
+
+export default YesSirStudio;
